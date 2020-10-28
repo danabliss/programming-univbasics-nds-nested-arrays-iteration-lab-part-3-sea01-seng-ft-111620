17c73f46fe232e168c7591d2bb77ac5d53b88468
Dana Bliss <danajackson2@gmail.com>
2020-10-28 17:44:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 def join_nested_strings(src)
   string = ""
   src.flatten.each {|item|
   if item.class == String
     string << "#{item} "
+    binding.pry
   end
   }
   puts string
