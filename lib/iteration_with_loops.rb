a8ea7c3512129100dc43bd24d7280be7f38f6017
Dana Bliss <danajackson2@gmail.com>
2020-10-28 17:45:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 
 
-def join_nested_strings(src)
-  string = ""
-  src.flatten.each {|item|
-  if item.class == String
-    string << "#{item} "
-  end
-  }
-  string
-end  
+#def join_nested_strings(src)
+#  string = ""
+#  src.flatten.each {|item|
+#  if item.class == String
+#    string << "#{item} "
+#  end
+#  }
+#  string
+#end  
