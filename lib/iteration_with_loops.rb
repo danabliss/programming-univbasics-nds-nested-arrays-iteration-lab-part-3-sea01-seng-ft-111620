58341d10a84f9b2d03e1df084ea6af3c5689b458
Dana Bliss <danajackson2@gmail.com>
2020-10-28 17:46:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,18 @@
-
+def join_nested_strings(src)
+  i = 0
+  sentence = ""
+  while i < src.length do
+    ii = 0
+    while ii < src[i].length do
+      if src[i][ii].class == String
+        sentence = sentence + " " + src[i][ii]
+      end  
+      ii += 1
+    end
+    i += 1
+  end  
+  sentence
+end
 
 #def join_nested_strings(src)
 #  string = ""
